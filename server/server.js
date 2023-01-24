80c0b8f8f71e5132cac301e943a9d7cfa0efb202
KawaManmi <kawamam2007@gmail.com>
2023-01-24 20:03:02 +0000

    It took me more than an hour to make the Nodjs to reaad both require and import, as I need it to import url-exist

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 const express = require("express");
 const app = express();
-onst cors = require("cors");
+import urlExist from "url-exist";
+// const cors = require("cors");
 const bp = require("body-parser");
 // const moment = require("moment");
 // var validator = require("email-validator");
-const bp = require("body-parser");
+
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,7 +18,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+// let videos = [];
 
 // GET "/"
 app.get("/", (req, res) => {
@@ -23,3 +26,12 @@ app.get("/", (req, res) => {
   // res.send({ express: "Your Backend Service is Running" });
   res.json({ videosData })
 });
+
+
+app.post("/", (req, res)=> {
+  if(!req.body.title ||  !req.body.url ){
+    res.status(400).send(`Title and url of the Video can't be empty`);
+    return;
+  }
+  else id
+})
