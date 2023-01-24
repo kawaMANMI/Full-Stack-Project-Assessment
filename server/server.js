d36453d6a04764c8c4fa1184b02b73dc560cc0c2
KawaManmi <kawamam2007@gmail.com>
2023-01-24 21:10:26 +0000

    The endpoint for adding video has been created, title and url must be not empty and not exist in the data, the url also valiate, date and time of adding will be added. Uniqe random id with 6 digits also created. All this have been tested using postman

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import { exists } from "fs";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const express = require("express");
@@ -13,7 +14,7 @@ app.use(bp.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(cors());
 const port = process.env.PORT || 5000;
-const videosData=require('./exampleresponse.json')
+let videosData = require("./exampleresponse.json");
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
@@ -24,14 +25,60 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
   // res.send({ express: "Your Backend Service is Running" });
-  res.json({ videosData })
+  res.json({ videosData });
 });
 
-
-app.post("/", (req, res)=> {
-  if(!req.body.title ||  !req.body.url ){
+app.post("/", (req, res) => {
+  if (!req.body.title || !req.body.url) {
     res.status(400).send(`Title and url of the Video can't be empty`);
     return;
+  } else {
+    let exists;
+    (async () => {
+       exists = await urlExist(req.body.url);
+      // Handle result
+      if (exists) {
+        // I know there are many options for creating videos id, but I would like make it as 6 digist and unique as follows
+        const arrayId = videosData.map((video) => parseInt(video.id));
+        let id = arrayId[0];
+        while (arrayId.includes(id)) {
+          id = Math.floor(100000 + Math.random() * 900000);
+        }
+
+        //check if url is already exist in your album
+        const arrayurl = videosData.map(
+          (video) => video.url );
+        if (arrayurl.includes(req.body.url)) {
+          res.status(400)
+          res.send(`url of this video already exist in your album`);
+          return;
+        }
+        //check if title is already exist in your album
+        const arraytitle = videosData.map(
+          (video) => video.title );
+        if (arraytitle.includes(req.body.title)) {
+          res.status(400).send(`This title alreay exist in your album`);
+          return;
+        }
+        let newVideo = {};
+        newVideo.id = id;
+        newVideo.title = req.body.title;
+        newVideo.url = req.body.url;
+        newVideo.rating = 0;
+        const now = new Date();
+        const date = now.toLocaleDateString();
+        const time = now.toLocaleTimeString();
+        newVideo.date = date;
+        newVideo.time = time;
+        videosData.push(newVideo);
+        res.json(videosData);
+        return;
+      } else {
+        res.status(400);
+        res.send(`Youtube URL is not exist`);
+        return;
+      }
+    })();
+    return;
   }
-  else id
-})
+});
